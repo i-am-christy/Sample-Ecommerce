0c284d031dd6cd647606d726741002aa5d2dae3c
LUNA <kunlechristy2019@gmail.com>
2025-05-25 21:13:02 +0100

    created a simple webpage to fetch pproducts from an API and display it on the webpage

---
+++ index.js
@@ -0,0 +1,35 @@
+const fetchBtn = document.getElementById("fetchBtn")
+fetchBtn.addEventListener("click", fetchProducts)
+
+async function fetchProducts() {
+    const response = await fetch("https://dummyjson.com/products")
+    
+    if(!response.ok){
+        throw new Error ("Failed to fetch products")
+    }
+    
+    const data = await response.json()
+    const products = data.products.slice(0,10);
+    displayProducts(products)
+}
+
+async function displayProducts(products) {
+    const productContainer =  document.getElementById("productContainer")
+    //clear previous html html content
+    productContainer.innerHTML = '';
+
+    //setTimeout(()=>{
+    products.forEach((product) =>{
+    //create a new element
+    const productDiv = document.createElement("div");
+    //set innerHTML attribute
+    productDiv.innerHTML = `
+            <img src="${product.thumbnail}">
+            <h3>${product.title}</h3>
+            <p>Price: $${product.price}</p>
+        `
+    //append each of the product attributes to the parent container
+    productContainer.appendChild(productDiv);
+    //}, 1500)
+    })
+}
